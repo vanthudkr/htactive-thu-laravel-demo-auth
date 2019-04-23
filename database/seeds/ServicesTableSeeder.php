7442d58b19aa6eb25be69b4712925531db35aa39
vanthudkr <thu.ho@student.passerellesnumeriques.org>
2019-04-23 09:08:05 +0700

    serviceseeder

--- database/seeds/ServicesTableSeeder.php
+++ database/seeds/ServicesTableSeeder.php
@@ -12,38 +12,39 @@ class ServicesTableSeeder extends Seeder
     public function run()
     {
         DB::table('services')->insert(
-        [
             [
-                'title' => 'Web Programing',
-                'content' => 'Outsourcing & web design on demand, not only business requirements but also technical requirements and system architecture.',
-                'image' => 'img/web-outsourcing.jpg',
+                [
+                    'title' => 'Web Programing',
+                    'content' => 'Outsourcing & web design on demand, not only business requirements but also technical requirements and system architecture.',
+                    'image' => 'img/web-outsourcing.jpg',
 
-            ],
-            [
-                'title' => 'Shopping website',
-                'content' => 'Generate a shopping website base on existing template with the best price.',
-                'image' => 'img/shopping-website.jpg'
-            ],
-            [
-                'title' => 'Portal',
-                'content' => 'Design and develop CMS sites, Portals for  education, business, medical...',
-                'image' => 'img/cms-site.jpg',
-            ],
-            [
-                'title' => 'Real estate',
-                'content' => 'Design and develop Websites for finding houses, real estate, house for rent...',
-                'image' => 'img/bds-site.jpg',
-            ],
-            [
-                'title' => 'Web maintenance',
-                'content' => 'Operating, maintenance, upgrade, repair, create more features... for exesting website.',
-                'image' => 'img/website-maintenance.jpg',
-            ],
-            [
-                'title' => 'Website hosting',
-                'content' => 'Operating, maintenance, upgrade, repair, create more features... for exesting website.',
-                'image' => 'img/hosting-cloud.jpg',
+                ],
+                [
+                    'title' => 'Shopping website',
+                    'content' => 'Generate a shopping website base on existing template with the best price.',
+                    'image' => 'img/shopping-website.jpg'
+                ],
+                [
+                    'title' => 'Portal',
+                    'content' => 'Design and develop CMS sites, Portals for  education, business, medical...',
+                    'image' => 'img/cms-site.jpg',
+                ],
+                [
+                    'title' => 'Real estate',
+                    'content' => 'Design and develop Websites for finding houses, real estate, house for rent...',
+                    'image' => 'img/bds-site.jpg',
+                ],
+                [
+                    'title' => 'Web maintenance',
+                    'content' => 'Operating, maintenance, upgrade, repair, create more features... for exesting website.',
+                    'image' => 'img/website-maintenance.jpg',
+                ],
+                [
+                    'title' => 'Website hosting',
+                    'content' => 'Operating, maintenance, upgrade, repair, create more features... for exesting website.',
+                    'image' => 'img/hosting-cloud.jpg',
+                ]
             ]
-        ]);
+        );
     }
 }
